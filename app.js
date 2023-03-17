b3dd4086337b7ec4a58c625cbf77d0888c43e936
thiagopoltronieri <thiago.poltronieri@outlook.com>
2023-03-17 18:02:08 -0300

    test_oauth_1mio_mct

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const request = require('request-promise');
+const axios = require('axios');
+const crypto = require('crypto');
 const app = express();
 const port = 3000;
 
@@ -14,43 +15,40 @@ const {
 } = process.env;
 
 app.get('/', (req, res) => {
-  const authURL = `${AUTHORIZATION_URL}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=profile`;
+  const state = crypto.randomBytes(16).toString('hex');
+  const authURL = `${AUTHORIZATION_URL}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=profile&state=${state}`;
   res.send(`<a href="${authURL}">Login with OAuth2</a>`);
 });
 
 app.get('/callback', async (req, res) => {
-  const { code } = req.query;
+  const { code, state } = req.query;
 
   try {
-    const tokenResponse = await request.post({
-      url: TOKEN_URL,
-      json: true,
-      form: {
-        grant_type: 'authorization_code',
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URI,
-        code
-      },
-      resolveWithFullResponse: true
+    const tokenResponse = await axios.post(TOKEN_URL, new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
+      code
+    }).toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     });
 
     console.log('Token response:', tokenResponse);
 
-    const { access_token } = tokenResponse.body;
+    const { access_token } = tokenResponse.data;
 
-    const userInfo = await request.get({
-      url: USERINFO_URL,
-      json: true,
+    const userInfo = await axios.get(USERINFO_URL, {
       headers: {
         'Authorization': `Bearer ${access_token}`
-      },
-      resolveWithFullResponse: true
+      }
     });
 
     console.log('User info response:', userInfo);
 
-    res.send(`<h1>Welcome ${userInfo.body.name}!</h1>`);
+    res.send(`<h1>Welcome ${userInfo.data.name}!</h1>`);
   } catch (error) {
     console.error(error);
     res.status(500).send(`Error during OAuth2 flow: ${error.message}`);
